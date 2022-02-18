22ec5fee213db1f16c7b8074c06a4c67b944bb0c
Jatin Narula <jatin.jt.narula@gmail.com>
2022-02-18 16:28:10 +0530

    Time: 3 ms (72.84%), Space: 6.5 MB (92.20%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string res = "";
+        
+        bool vis[26] = {false};
+        int last[26] = {};
+        
+        for (int i = 0; i < s.length(); i ++) 
+            last[s[i] - 'a'] = i;
+        
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (vis[s[i] - 'a'])
+                continue;
+            while (res.size() && res[res.size() - 1] > s[i] && last[res[res.size() - 1] - 'a'] > i) {
+                vis[res[res.size() - 1] - 'a'] = false;
+                res.pop_back();
+            }
+            
+            res.push_back(s[i]);
+            vis[s[i] - 'a'] = true;
+        }
+        
+        return res;
+    }
+};
