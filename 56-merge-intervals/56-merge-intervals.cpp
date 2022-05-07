b514514a444f2591a4c2bb19314eb3395e29bd1c
Jatin Narula <jatin.jt.narula@gmail.com>
2022-05-07 07:18:37 +0530

    Time: 109 ms (11.26%), Space: 19.1 MB (67.33%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     
-    bool isOverlap(int x1, int y1, int x2, int y2) {
-        return (min(y1, y2) - max(x1, x2)) >= 0;
+    bool isOverlap(int a, int b, int x, int y) {
+        return (min(b,y) - max(a, x)) >= 0;
     }
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        int n = intervals.size();
         
-        if (n == 1) 
-            return intervals;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         
         vector<vector<int>> res;
-        int min_x, max_y;
-        for (int i = 1; i < n; i++) {
-            int x1 = intervals[i - 1][0];
-            int y1 = intervals[i - 1][1];
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            int x = intervals[i][0];
+            int y = intervals[i][1];
             
-            min_x = x1, max_y = y1;
-            while (i < n && isOverlap(min_x, max_y, intervals[i][0], intervals[i][1])) {
-                min_x = min(min_x, intervals[i][0]);
-                max_y = max(max_y, intervals[i][1]);
-                i++;
+            if (isOverlap(start, end, x, y)) {
+                start = min(x, start);
+                end = max(y, end);
+            } else {
+                res.push_back({start, end});
+                start = x;
+                end = y;
             }
-            
-            cout << min_x  << ", " << max_y << endl;
-            res.push_back({min_x, max_y});
         }
         
-        if (!isOverlap(min_x, max_y, intervals[n - 1][0], intervals[n - 1][1]))
-            res.push_back({intervals[n - 1][0], intervals[n - 1][1]});
-        
+        res.push_back({start, end});
         return res;
-        
     }
 };
