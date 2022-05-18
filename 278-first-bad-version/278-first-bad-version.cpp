97cde8aee891df22597754d083926c8330b53e33
Jatin Narula <jatin.jt.narula@gmail.com>
2022-05-18 21:37:38 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (89.85%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int low = 0, high = n;
+        int res = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (isBadVersion(mid)) {
+                high = mid - 1;
+                res = mid;
+            } else
+                low = mid + 1;
+        }
+        
+        return res;
+    }
+};
